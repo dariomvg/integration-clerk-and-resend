59f80dc8f17eaf7fa9cae4ded8d274d1b0895d7b
dariomvg <contact.dario.info@gmail.com>
2026-09-03 16:50:21 -0300

    fix proxy.ts: Add frontendApiProxy: enable true

--- src/proxy.ts
+++ src/proxy.ts
@@ -13,7 +13,11 @@
 
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+export default clerkMiddleware({
+  frontendApiProxy: {
+    enabled: true,
+  },
+});
 
 export const config = {
   matcher: [
@@ -21,5 +25,7 @@ export const config = {
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes.
     "/(api|trpc)(.*)",
+     // Always run for Clerk's Frontend API proxy.
+    "/__clerk/(.*)",
   ],
 };
